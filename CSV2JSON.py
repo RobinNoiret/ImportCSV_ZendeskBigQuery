795dfb9320114ea381a6c2378f03b3dd715a7dc4
Robin Noiret <robin.noiret2@gmail.com>
2024-05-24 11:12:54 +0200

    New version : - converter code - insert code

--- CSV2JSON.py
+++ CSV2JSON.py
@@ -1,21 +1,23 @@
+print("... Libraries loading ...")
 import csv
 import json
+print("... Libraries loading done ...")
 
 def csv_to_json(csv_file_path, json_file_path, delimiter=';'):
-    # Lire le fichier CSV avec le délimiteur approprié
+    # Read the CSV file with the appropriate delimiter
     data = []
     with open(csv_file_path, encoding='utf-8') as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=delimiter)
         for row in csv_reader:
             data.append(row)
     
-    # Sauvegarder les données JSON dans un fichier
+    # Save JSON data to a file 
     with open(json_file_path, 'w', encoding='utf-8') as json_file:
         json.dump(data, json_file, indent=4)
     
-    print(f"Les données ont été converties de {csv_file_path} à {json_file_path}")
+    print(f"Data have been converted from {csv_file_path} to {json_file_path}")
 
-# Chemin vers le fichier CSV et le fichier JSON de sortie
+# Path to output CSV file and JSON file
 csv_file_path = 'input.csv'
 json_file_path = 'output.json'
 
