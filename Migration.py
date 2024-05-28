dd9835e1c3cddffb960ee9a0f03a2de9c3114495
Robin Noiret <robin.noiret2@gmail.com>
2024-05-28 14:35:39 +0200

    Files updates

--- Migration.py
+++ Migration.py
@@ -16,6 +16,7 @@ print("... Bibliothèques chargées ...")
 # ____________________________________________________________________________________________________________________________________________________________________ #
 
 
+
 #project_id = "your_projectID"                               # BigQuery project identifier
 #dataset_id = "your_datasetID"                               # BigQuery dataset identifier
 #table_name = "your_tablename"                               # Destination table name
